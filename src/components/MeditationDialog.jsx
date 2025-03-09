71a8f230c9d1de00560855e8eb4c83e2bc16ccfe
Jwong105 <jennifer.wong105@gmail.com>
2025-03-08 20:30:51 -0800

    meditate

--- src/components/MeditationDialog.jsx
+++ src/components/MeditationDialog.jsx
@@ -5,16 +5,22 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Box from '@mui/material/Box';
+import Countdown from './Countdown';
+import {useState} from 'react';
 
 // eslint-disable-next-line react/prop-types
 export default function AlertDialog({ open, onClose }) {
+  const [isReady, setIsReady] = useState(false);
+
     const handleClose = () => {
         onClose();
     }
 
+    const handleTimer = () => {
+      setIsReady(true);
+    }
+
     return (
-      <div>
-      <logoImg></logoImg>
       <Dialog
         open={open}
         onClose={onClose}
@@ -25,10 +31,11 @@ export default function AlertDialog({ open, onClose }) {
                   position: 'absolute', 
                   top: '50%', 
                   left: '50%', 
-                  transform: 'translate(-50%, -50%)' 
+                  transform: 'translate(-50%, -50%)'
               }
           }}
       >
+      { !isReady ? (
         <Box sx={{backgroundColor: "#ece3ca"}}>
           <DialogTitle sx={{color: "#793205", fontFamily: "Arial, sans-serif", fontWeight: 800}}>
               {"Practice Deep Breathing"}
@@ -44,12 +51,14 @@ export default function AlertDialog({ open, onClose }) {
                 {"Another Time"}
             </Button>
             <Button sx={{width: "150px", color: "#793205", fontFamily: "Arial, sans-serif", fontWeight: 800, backgroundColor: '#dbca9b', "&:hover": {
-               backgroundColor: "#c2b693"},}} onClick={handleClose}>
+               backgroundColor: "#c2b693"},}} onClick={handleTimer}>
                 {"I'm Ready"}
             </Button>
           </DialogActions>
         </Box>
+      ) : (
+        <Countdown onClose={onClose}></Countdown>
+      )}
       </Dialog>
-      </div>
     );
   }
